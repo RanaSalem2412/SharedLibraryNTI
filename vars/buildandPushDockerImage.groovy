92ec4eb8cc53c3afe3b96a32377e3c43193a95d3
RanaSalem2412 <ranasalem923@gmail.com>
2025-03-18 08:13:34 +0200

    Create buildandPushDockerImage.groovy

---
+++ vars/buildandPushDockerImage.groovy
@@ -0,0 +1,9 @@
+#!usr/bin/env groovy
+def call(String dockerHubCredentialsID, String repoName, String imageName) {
+	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+		sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+        	sh "docker build -t ${repoName}/${imageName}:${BUILD_NUMBER} ."
+        	sh "docker push ${repoName}/${imageName}:${BUILD_NUMBER}"	 
+        	sh "docker rmi ${repoName}/${imageName}:${BUILD_NUMBER}"
+  }
+}

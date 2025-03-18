45cc6c87f5fbcd0c88cd868e7c2e050fdbfdc650
RanaSalem2412 <ranasalem923@gmail.com>
2025-03-18 16:40:42 +0200

    Update deployOnKubernetes.groovy

--- vars/deployOnKubernetes.groovy
+++ vars/deployOnKubernetes.groovy
@@ -1,10 +1,13 @@
-def deployOnKubernetes(k8sCredentialsID, deploymentFile) {
+def call(Map args) {
+    def k8sCredentialsID = args.k8sCredentialsID
+    def deploymentFile = args.deploymentFile
+
     withCredentials([file(credentialsId: k8sCredentialsID, variable: 'KUBECONFIG')]) {
         sh '''
             echo "KUBECONFIG is set to: ${KUBECONFIG}"
             export KUBECONFIG=${KUBECONFIG}
             kubectl cluster-info
             kubectl apply -f ${deploymentFile}
-        '''
+        ''' 
     }
 }
